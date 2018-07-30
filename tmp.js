7254acbd87b87c762a5250917d9f132548916420
Luu Ninh <ninhxuanluu@gmail.com>
2018-07-30 09:57:10 +0700

    add tmp.js

---
+++ tmp.js
@@ -0,0 +1,20 @@
+const ids = [
+  1281929, // verik-iot
+  5004286, // alexa-homeskill
+  4449540, // gaction-smarthome
+  4836986, // customskill
+  1823561, // venus-customskill-intergration
+  4781483, // gaction-customs
+  3686842, // Wowza module
+  3171321, // devices
+  2248416, // venus-devices
+  2285501, // fota-server
+  1711596, // venus-dashboard-user
+  6378989, // zino-api
+  6368238, // zinno-website
+  6333313, // messenger-bot
+  7441018, // zinno/backend/zinno-request
+  7655092, // zinno/backend/alexa-smarthome-skill
+  7393641, // zinno_sw/zinno_app/device_db
+];
+console.log('node reports.js -w 29 -p ', ids.join(','), ' -t EMXyKW-KxtAqNG-hJDGS');
